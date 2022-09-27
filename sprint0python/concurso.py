2e8d207731c54239007162d77e6129f4c13ec8c6
PLMV <pmalavevfp@gmail.com>
2022-09-27 09:39:51 +0200

    Terminado 2do ejercicio de python

--- sprint0python/concurso.py
+++ sprint0python/concurso.py
@@ -1,4 +1,8 @@
-print ("¿Cúal es segundo país con más campos de golf?")
+print ("Concurso de Preguntas")
+print ()
+print ("Pregunta No. 1")
+print ("")
+print ("¿Cúal es el segundo país con más campos de golf?")
 print ("Las opciones son :")
 print ()
 print ("a.- Estados Unidos")
@@ -8,7 +12,41 @@ print ()
 ans=input ("Introduce tu respuesta = ??")
 
 if ans != "b":
-	print ("Tu respuesta ha sido Incorrecta")
+	scr=-5
 else :
-	print ("Tu respuesta es Correcta")
+	scr=10
 
+print ("Pregunta No. 2")
+print ("")
+print ("¿Las células humanas son del tipo?")
+print ("Las opciones son :")
+print ()
+print ("a.- Eucariota")
+print ("b.- Procariota")
+print ("c.- Ninguna de las Anteriores")
+print ()
+ans=input ("Introduce tu respuesta = ??")
+
+if ans != "a":
+	scr+=+5
+else :
+	scr+=10
+	
+print ("Pregunta No. 3")
+print ("")
+print ("¿La Provincia de A Coruña se encuentra en ?")
+print ("Las opciones son :")
+print ()
+print ("a.- Madrid")
+print ("b.- Andalucia")
+print ("c.- Galicia")
+print ()
+ans=input ("Introduce tu respuesta = ??")
+
+if ans != "c":
+	scr+=-5
+else :
+	scr+=10
+
+print ()
+print ("Tu puntuación en el cuestionario es " + str(scr))
