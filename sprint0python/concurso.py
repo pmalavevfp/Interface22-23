2c01c1376ede4e44b18f695ae51a0caa7c072458
PLMV <pmalavevfp@gmail.com>
2022-09-27 09:17:08 +0200

    terminado 1er ejercicio de python

---
+++ sprint0python/concurso.py
@@ -0,0 +1,14 @@
+print ("¿Cúal es segundo país con más campos de golf?")
+print ("Las opciones son :")
+print ()
+print ("a.- Estados Unidos")
+print ("b.- Japon")
+print ("c.- Inglatera")
+print ()
+ans=input ("Introduce tu respuesta = ??")
+
+if ans != "b":
+	print ("Tu respuesta ha sido Incorrecta")
+else :
+	print ("Tu respuesta es Correcta")
+
