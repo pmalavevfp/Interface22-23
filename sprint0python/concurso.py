ecc1c326f922ea0f2cdb04cde07d299c3f221d77
PLMV <pmalavevfp@gmail.com>
2022-09-28 09:36:12 +0200

    Completada tarea 7

--- sprint0python/concurso.py
+++ sprint0python/concurso.py
@@ -1,52 +1,73 @@
+from random import randrange
+
 print ("Concurso de Preguntas")
 print ()
-print ("Pregunta No. 1")
-print ("")
-print ("¿Cúal es el segundo país con más campos de golf?")
-print ("Las opciones son :")
-print ()
-print ("a.- Estados Unidos")
-print ("b.- Japon")
-print ("c.- Inglatera")
-print ()
-ans=input ("Introduce tu respuesta = ??")
+scr=0
 
-if ans != "b":
-	scr=-5
-else :
-	scr=10
 
-print ("Pregunta No. 2")
-print ("")
-print ("¿Las células humanas son del tipo?")
-print ("Las opciones son :")
-print ()
-print ("a.- Eucariota")
-print ("b.- Procariota")
-print ("c.- Ninguna de las Anteriores")
-print ()
-ans=input ("Introduce tu respuesta = ??")
+qst1 = randrange(1, 4)
+qst2 =qst1
 
-if ans != "a":
-	scr+=+5
-else :
-	scr+=10
-	
-print ("Pregunta No. 3")
-print ("")
-print ("¿La Provincia de A Coruña se encuentra en ?")
-print ("Las opciones son :")
-print ()
-print ("a.- Madrid")
-print ("b.- Andalucia")
-print ("c.- Galicia")
-print ()
-ans=input ("Introduce tu respuesta = ??")
+while (qst2==qst1):
+	qst2=randrange (1,4)
 
-if ans != "c":
-	scr+=-5
-else :
-	scr+=10
+if (qst1==1) | (qst2==1):
+
+	print ("Pregunta No. 1")
+	print ("")
+	print ("¿Cúal es el segundo país con más campos de golf?")
+	print ("Las opciones son :")
+	print ()
+	print ("a.- Estados Unidos")
+	print ("b.- Japon")
+	print ("c.- Inglatera")
+	print ()
+	ans=input ("Introduce tu respuesta = ??")
+
+	if ans != "b":
+		print("Tu respuesta ha sido Incorrecta")
+		scr += -5
+	else:
+		print("Tu respuesta ha sido Correcta")
+		scr += 10
+
+if (qst1==2) | (qst2==2):
+	print ("Pregunta No. 2")
+	print ("")
+	print ("¿Las células humanas son del tipo?")
+	print ("Las opciones son :")
+	print ()
+	print ("a.- Eucariota")
+	print ("b.- Procariota")
+	print ("c.- Ninguna de las Anteriores")
+	print ()
+	ans=input ("Introduce tu respuesta = ??")
+
+	if ans != "a":
+		print("Tu respuesta ha sido Incorrecta")
+		scr += -5
+	else:
+		print("Tu respuesta ha sido Correcta")
+		scr += 10
+
+if (qst1==3) | (qst2==3):
+	print ("Pregunta No. 3")
+	print ("")
+	print ("¿La Provincia de A Coruña se encuentra en ?")
+	print ("Las opciones son :")
+	print ()
+	print ("a.- Madrid")
+	print ("b.- Andalucia")
+	print ("c.- Galicia")
+	print ()
+	ans=input ("Introduce tu respuesta = ??")
+
+	if ans != "c":
+		print ("Tu respuesta ha sido Incorrecta")
+		scr+=-5
+	else :
+		print("Tu respuesta ha sido Correcta")
+		scr+=10
 
 print ()
-print ("Tu puntuación en el cuestionario es " + str(scr))
+print ("Tu puntuación en el cuestionario es de " + str(scr)+" ptos-")
