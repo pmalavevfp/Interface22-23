9d81d9aef92fe8b2c5961e8cf1cfb6c8159d2bff
PLMV <pmalavevfp@gmail.com>
2022-09-28 09:12:06 +0200

    Completada tarea 6

--- sprint0python/principal.py
+++ sprint0python/principal.py
@@ -1,6 +1,7 @@
 from fibonacci import funcion_fibonacci
 
 from fibonacci2 import funcion_fibonacci2
+import time
 
 
 print ("Para calcular el valor de la posición la serie de fibonacci")
@@ -12,8 +13,15 @@ select=input ("Qué modo se va aplicar? ---> ")
 numbFine = int (input ("Qué posición de la serie de fibonacci quieres ver  -->  "))
 
 if select =="a":
+    start_time = time.time()
     scr = funcion_fibonacci (numbFine)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 else:
+    start_time = time.time()
     scr = funcion_fibonacci2 (numbFine)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
 
 print ("El número de fibonacci para la posición "+ str(numbFine) + " es ==> " + str(scr))
+print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' seg')
