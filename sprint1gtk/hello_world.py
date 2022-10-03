0ffffe24a3b07d89d82faa86e0f1b620bbb826f9
PLMV <pmalavevfp@gmail.com>
2022-10-03 12:16:01 +0200

    Completado el ejercicio 1, del sprint1 y luedo de entender el ejercicio

--- sprint1gtk/hello_world.py
+++ sprint1gtk/hello_world.py
@@ -1,1 +1,8 @@
-print ("Hola Mundo")
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+window = Gtk.Window(title="Hola Mundo")
+window.show()
+window.connect("destroy", Gtk.main_quit)
+Gtk.main()
