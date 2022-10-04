d3c055f60f4f9a3adf5bb79619f601d896a855e2
PLMV <pmalavevfp@gmail.com>
2022-10-04 08:42:40 +0200

    Completada tarea 3 del ejercicio 1,1

--- sprint1gtk/dialog/window.py
+++ sprint1gtk/dialog/window.py
@@ -1,16 +1,40 @@
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
+from yes_window import YesWin
+from no_window import NoWin
 
 class MainWindow(Gtk.Window):
-    button=Gtk.Button(label="Realizar acción")
+    box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    box2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+    box3 = Gtk.Box (orientation=Gtk.Orientation.VERTICAL)
+    label = Gtk.Label("Entiendes lo que estas haciendo?????")
+    button1 = Gtk.Button(label="SI")
+    button2 = Gtk.Button(label="NO")
 
     def __init__ (self):
         super ().__init__(title="Main")
         self.connect ("destroy",  Gtk.main_quit)
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
+        #self.button1.connect("clicked", self.on_button_clicked)
+        self.add(self.box)
+        self.box.pack_start(self.label, True, True, 0)
+        self.box.pack_start(self.box2, True, True, 0)
+        self.box2.pack_start(self.button1, True, True, 0)
+        self.button1.connect("clicked", self.on_button1_clicked)
+        self.box2.pack_start(self.button2, True, True, 0)
+        self.button2.connect("clicked", self.on_button2_clicked)
 
-    def on_button_clicked (self, widget):
-        pass
+    def on_button1_clicked (self, widget):
+        win = YesWin()
+        win.show_all()
+        #labelY = Gtk.Label("Presionaste si")
+        #self.box2.pack_start(self.box3, True, True, 0)
+        #self.box3.pack_start(self.labelY, True, True, 0)
+        #print ("SI")
+    def on_button2_clicked (self, widget):
+        win=NoWin ()
+        win.show_all()
+        self.box2.pack_start(self.box3, True, True, 0)
+        print("NO")
+
 
---
+++ sprint1gtk/dialog/yes_window.py
@@ -0,0 +1,14 @@
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
+class YesWin (Gtk.Window):
+    box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+    label=Gtk.Label("Has clickado un Si")
+
+
+    def __init__ (self):
+        super().__init__(title="Respuesta")
+        self.add(self.box)
+        self.box.pack_start(self.label, True, True, 0)
+
