bae6c3cc66fdd03fd8ee9b6570d9389b32e7a1ba
PLMV <pmalavevfp@gmail.com>
2022-10-05 10:05:30 +0200

    Completada tarea 6 del sprint1

--- sprint1gtk/catalog/window.py
+++ sprint1gtk/catalog/window.py
@@ -3,6 +3,8 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from cell import Cell
+from gi.repository import Gtk, Gdk, GdkPixbuf
+from gi.repository.GdkPixbuf import Pixbuf
 
 
 class MainWindow(Gtk.Window):
@@ -25,14 +27,41 @@ class MainWindow(Gtk.Window):
         scrolled.add(self.flowbox)
         self.add(scrolled)
 
-        
-        cell_one = Cell("Robin", Gtk.Image.new_from_file("data/edited/robin.png"))
+        #cell_one = Cell("Goku", Gtk.Image.new_from_file("data/unedited/Goku.jfif"))
+        goku = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/Goku.jfif", 200, 200, False)
+        goku.set_from_pixbuf(pixbuf)
+        cell_one=Cell("GoKu", goku)
         self.flowbox.add(cell_one)
-        cell_two = Cell("Mickey", Gtk.Image.new_from_file("data/edited/Mickey-Mouse.png"))
+
+
+        #cell_two = Cell("Loquillo", Gtk.Image.new_from_file("data/unedited/images.jfif"))
+        loqui = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/images.jfif", 200, 200, False)
+        loqui.set_from_pixbuf(pixbuf)
+        cell_two=Cell("Loquillo", loqui)
         self.flowbox.add(cell_two)
-        cell_three = Cell("Aventure", Gtk.Image.new_from_file("data/edited/adventuretime-200x200.png"))
+
+
+        #cell_three = Cell("Batman", Gtk.Image.new_from_file("data/unedited/images (1).jfif"))
+        bat = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/images (1).jfif", 200, 200, False)
+        bat.set_from_pixbuf(pixbuf)
+        cell_three = Cell("Batman", bat)
         self.flowbox.add(cell_three)
-        cell_four = Cell("Tom&Jerry", Gtk.Image.new_from_file("data/edited/tom&Jerry.jpg"))
+
+
+        #cell_four = Cell("Goku", Gtk.Image.new_from_file("data/unedited/cojin-3d-goku-dragon-ball.jpg"))
+        gok = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/cojin-3d-goku-dragon-ball.jpg", 200, 200, False)
+        gok.set_from_pixbuf(pixbuf)
+        cell_four = Cell("GoKu", gok)
         self.flowbox.add(cell_four)
-        cell_five = Cell("Bob", Gtk.Image.new_from_file("data/edited/Bob.png"))
+
+
+        #cell_five = Cell("Natuto", Gtk.Image.new_from_file("data/unedited/naruto.jpg"))
+        naru = Gtk.Image()
+        pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale("data/unedited/naruto.jpg", 200, 200, False)
+        naru.set_from_pixbuf(pixbuf)
+        cell_five = Cell("Naruto", naru)
         self.flowbox.add(cell_five)
