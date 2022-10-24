2eccd3f77998c2b5100bb656e9f3a2a79680b029
PLMV <pmalavevfp@gmail.com>
2022-10-24 09:59:16 +0200

    Completada tarea 3 del 2do. sprint

--- 2sprint (sprint2catalog)/load_window.py
+++ 2sprint (sprint2catalog)/load_window.py
@@ -1,10 +1,10 @@
 # Generando la class de LoanWindow para generar la ventana del proyecto
 
 import gi
-
+import requests, threading, shutil
 gi.require_version("Gtk","3.0")
 from gi.repository import Gtk, GLib
-
+from window import MainWindow
 
 class LoadWindow (Gtk.Window):
     label = Gtk.Label("Cargando elementos ...")
@@ -26,3 +26,45 @@ class LoadWindow (Gtk.Window):
         self.box.pack_start(self.label, False, False, 0)
         self.box.pack_start(self.spinner, False, False, 0)
         self.add(self.box)
+
+        self.launch_load ()
+
+    def start_main_window(self, loaded_items_list):
+        win = MainWindow(loaded_items_list)
+        win.show_all()
+        self.disconnect_by_func(Gtk.main_quit())
+        self.close()
+
+    """Para la petición HTTP desde un hilo secundario. Para esto Usamos la librería 
+“threading” para ello"""
+
+    def launch_load (self):
+        thread=threading.Thread(target=self.load_json, args=())
+        thread.start()
+
+    """para obtener el archivo JSON de la URL del API-Rest. basta para enviar la 
+    petición GET, que se encapsula a través de esta función """
+    def load_json (self):
+        response=requests.get ("https://raw.githubusercontent.com/pmalavevfp/Interface22-23/main/API-REST/catalog.json")
+        json_list=response.json()
+
+        """Se crea esta rutina para crear una nueva lista con los datos de URL de la imagen
+    la descripción y el nombre de la figura"""
+
+        #se crea una lista
+        result =[]
+
+        for json_item in json_list:
+            name=json_item.get ("name")
+            description=json_item.get ("descripcion")
+            image_url= json_item.get("image_url")
+            r=requests.get(image_url,stream=True)
+            with open ("temp.png", "wb") as f:
+                shutil.copyfileobj(r.raw,f)
+            image=Gtk.Image.new_from_file ("temp.png")
+            result.append({"name":name, "description":description, "gtk_image":image})
+
+            #pasando la lista de resultado al hilo ppal
+
+            GLib.idle_add(self.start_main_window, result)
+
