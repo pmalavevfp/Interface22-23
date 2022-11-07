7777e8eff58a914c195d8bfadd8717eadc9b7a96
PLMV <pmalavevfp@gmail.com>
2022-11-07 17:03:34 +0100

    Completada 2da tarea del 3 sprint

--- 3sprint-android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
+++ 3sprint-android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.catalogactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context=this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button nav_detail = findViewById(R.id.detail);
+        nav_detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (context, DetailActivity.class);
+                context.startActivity(intent);
+            }
+        });
     }
+
+
 }
