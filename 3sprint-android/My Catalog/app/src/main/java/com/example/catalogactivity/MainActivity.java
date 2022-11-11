4c8f68a3291fa48ee56c102eaaa04d13ff2b263f
PLMV <pmalavevfp@gmail.com>
2022-11-11 13:45:28 +0100

    Completada tarea 5 del 3er sprint

--- 3sprint-android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
+++ 3sprint-android/My Catalog/app/src/main/java/com/example/catalogactivity/MainActivity.java
@@ -1,32 +1,100 @@
 package com.example.catalogactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private Context context=this;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+    private Menu menu;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.navigator_layout);
+        toolbar=findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
 
-        Button nav_detail = findViewById(R.id.detail);
+        menu=navigationView.getMenu();
+        navigationView.getHeaderView(0);
+        navigationView.bringToFront();
+
+
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.home);
+
+
+
+
+        /*Button nav_detail = findViewById(R.id.detail);
         nav_detail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent (context, DetailActivity.class);
                 context.startActivity(intent);
             }
-        });
+        });*/
+
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                drawerLayout.closeDrawer(GravityCompat.START);
+                break;
+           case R.id.activity_2:
+                startActivity(new Intent(MainActivity.this,AboutActivity.class));
+                break;
 
+            case R.id.activity_1:
+                Intent i = new Intent(this, DetailActivity.class);
+                startActivity(i);
+                break;
+            case R.id.help:
+                // Activity Help
+                break;
+            case R.id.privacy:
+                ///Activity Privacy
+                String url="https://www.faconnable.com/en_eu/privacy-policy.html";
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.android.chrome");
+                try {
+                    getApplicationContext().startActivity(intent);
+                } catch (ActivityNotFoundException exception) {
+                    intent.setPackage(null);
+                    getApplicationContext().startActivity(intent);
+                }
+                break;
+        }
+
+        return false;
+    }
 }
