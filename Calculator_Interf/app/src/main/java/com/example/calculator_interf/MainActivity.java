d8ec48cb63b41af5b1f38ba73e2b5d5308450ac7
PLMV <pmalavev@fpcoruna.afundacion.org>
2022-12-13 10:08:31 +0100

    El xml de la calculadora esta terminado

--- Calculator_Interf/app/src/main/java/com/example/calculator_interf/MainActivity.java
+++ Calculator_Interf/app/src/main/java/com/example/calculator_interf/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.calculator_interf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView screen;
+    private String input_1, input_2, input_3, input_4, answer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        screen=findViewById(R.id.screen);
+    }
+
+    public void onClick(View v) {
+                break;
+            case (R.id.eex):
+                break;
+            case (R.id.clx):
+                break;
+            case (R.id.one):
+                break;
+            case (R.id.two):
+                break;
+            case (R.id.three):
+                break;
+            case (R.id.four):
+                break;
+            case (R.id.five):
+                break;
+            case (R.id.six):
+                break;
+            case (R.id.seven):
+                break;
+            case (R.id.eight):
+                break;
+            case (R.id.nine):
+                break;
+            case (R.id.zero):
+                break;
+            case (R.id.dot):
+                break;
+            case (R.id.r_s):
+                break;
+            case (R.id.subt):
+                break;
+            case (R.id.plus):
+                break;
+            case (R.id.mult):
+                break;
+            case (R.id.div):
+                break;
+
+        }
+
     }
 }
